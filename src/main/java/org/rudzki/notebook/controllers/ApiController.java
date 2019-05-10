d85c8b2d4f913fee1bc148487ec88a4116c5f2d6
Chris Rudzki <chris@rudzki.org>
2019-05-10 16:30:21 -0400

    publish and delete apis

--- src/main/java/org/rudzki/notebook/controllers/ApiController.java
+++ src/main/java/org/rudzki/notebook/controllers/ApiController.java
@@ -1,12 +1,16 @@
 package org.rudzki.notebook.controllers;
 
 import java.util.List;
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.rudzki.notebook.data.NoteDao;
 import org.rudzki.notebook.models.Note;
@@ -23,15 +27,28 @@ public class ApiController {
 		List<Note> notes = dao.getAllNotes();
 		return notes;
 	}
-	
+
 	@GetMapping("/note/{id}/")
 	public Note displayNote(HttpServletRequest req, @PathVariable("id") String id) {
 		return dao.getNote(Long.parseLong(id));
 	}
-	
-//	@GetMapping("/site-info/")
-//	public String displayInfo() {
-//		dao.
-//	}
-//	dao.get
+
+	@GetMapping("/note/{id}/delete/")
+	public void deleteNote(HttpServletRequest req, @PathVariable("id") String id) {
+		try {
+			dao.deleteNote(Long.parseLong(id));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@PostMapping("/note/add/")
+	public void addNote(@RequestParam("title") String title,
+						@RequestParam("content") String content,
+						@RequestParam("slug") String slug) {
+		Note note = new Note();
+		note.setTitle(title);
+		note.setContent(content);
+		dao.addNote(note);
+	}
 }
